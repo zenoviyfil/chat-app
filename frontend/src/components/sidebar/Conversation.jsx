a3e95dfea8bcdc2eefa508a70bb686a7bff9265d
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-09 15:28:00 +0100

    UI building

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,19 @@
+const Conversation = () => {
+  return (
+    <>
+        <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+            <div className="avatar online">
+                <div className="w-12 rounded-full">
+                    <img src="" alt="user avatar" />
+                </div>
+            </div>
+            <div className="flex flex-col flex-1">
+                
+            </div>
+        </div>
+        <div></div>
+    </>
+  )
+}
+
+export default Conversation
---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,11 @@
+import Conversation from "./Conversation"
+
+const Conversations = () => {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+        <Conversation />
+    </div>
+  )
+}
+
+export default Conversations
