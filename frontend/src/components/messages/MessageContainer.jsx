9f210c239583648379810cfe6d41d644fb6df517
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-14 18:35:02 +0100

    Revert "failed"
    This reverts commit 14f344193c23f389666bf97b0fbaa289bde3cd2e.

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,68 +1,61 @@
-import { useEffect } from "react";
-import useConversation from "../../zustand/useConversation";
-import MessageInput from "./MessageInput";
-import Messages from "./Messages";
-import { TiMessages } from "react-icons/ti";
-import { useAuthContext } from "../../context/AuthContext";
+import MessageInput from "./MessageInput"
+import Messages from "./Messages"
+import { TiMessages } from "react-icons/ti"
 
 const MessageContainer = () => {
-	const { selectedConversation, setSelectedConversation } = useConversation();
+  const noChatSelected = true;
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+        {noChatSelected ? (<NoChatSelected />
+        ) : (
+        <>
+            <div className="bg-slate-500 px-4 py-2 mb-2">
+                <span className="label-text">To:</span>
+                <span className="text-gray-900 font-bold">John Doe</span>
+            </div>
 
-	useEffect(() => {
-		// cleanup function (unmounts)
-		return () => setSelectedConversation(null);
-	}, [setSelectedConversation]);
+            <Messages />
+            <MessageInput />
+        </>)}
+    </div>
+  )
+}
 
-	return (
-		<div className='md:min-w-[450px] flex flex-col'>
-			{!selectedConversation ? (
-				<NoChatSelected />
-			) : (
-				<>
-					{/* Header */}
-					<div className='bg-slate-500 px-4 py-2 mb-2'>
-						<span className='label-text'>To:</span>{" "}
-						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
-					</div>
-					<Messages />
-					<MessageInput />
-				</>
-			)}
-		</div>
-	);
-};
-export default MessageContainer;
+export default MessageContainer
+
 
 const NoChatSelected = () => {
-	const { authUser } = useAuthContext();
-	return (
-		<div className='flex items-center justify-center w-full h-full'>
-			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-				<p>Welcome 👋 {authUser.fullName} ❄</p>
-				<p>Select a chat to start messaging</p>
-				<TiMessages className='text-3xl md:text-6xl text-center' />
-			</div>
-		</div>
-	);
-};
+    return (
+        <div className="flex items-center justify-center w-full h-full">
+            <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+                <p>Welcome John Doe</p>
+                <p>Select a chat to start messaging</p>
+                <TiMessages className='text-3xl md:text-6xl text-center' />
+            </div>
+        </div>
+    )
+}
 
-// STARTER CODE SNIPPET
-// import MessageInput from "./MessageInput";
-// import Messages from "./Messages";
+
+// STARTER CODE
+
+// import MessageInput from "./MessageInput" 
+// import Messages from "./Messages"
 
 // const MessageContainer = () => {
-// 	return (
-// 		<div className='md:min-w-[450px] flex flex-col'>
-// 			<>
-// 				{/* Header */}
-// 				<div className='bg-slate-500 px-4 py-2 mb-2'>
-// 					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
-// 				</div>
+//   return (
+//     <div className="md:min-w-[450px] flex flex-col">
+//         <>
+//             <div className="bg-slate-500 px-4 py-2 mb-2">
+//                 <span className="label-text">To:</span>
+//                 <span className="text-gray-900 font-bold">John Doe</span>
+//             </div>
 
-// 				<Messages />
-// 				<MessageInput />
-// 			</>
-// 		</div>
-// 	);
-// };
-// export default MessageContainer;
+//             <Messages />
+//             <MessageInput />
+//         </>
+//     </div>
+//   )
+// }
+
+// export default MessageContainer
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,37 +1,25 @@
-import useGetConversations from "../../hooks/useGetConversations";
-import Conversation from "./Conversation";
+import Conversation from "./Conversation"
 
 const Conversations = () => {
-	const { loading, conversations } = useGetConversations();
-	return (
-		<div className='py-2 flex flex-col overflow-auto'>
-			{conversations.map((conversation, idx) => (
-				<Conversation
-					key={conversation._id}
-					conversation={conversation}
-					lastIdx={idx === conversations.length - 1}
-				/>
-			))}
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+        <Conversation />
+    </div>
+  )
+}
 
-			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
-		</div>
-	);
-};
-export default Conversations;
+export default Conversations
 
-// STARTER CODE SNIPPET
-// import Conversation from "./Conversation";
+
+// STARTER CODE
+// import Conversation from "./Conversation"
 
 // const Conversations = () => {
-// 	return (
-// 		<div className='py-2 flex flex-col overflow-auto'>
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 		</div>
-// 	);
-// };
-// export default Conversations;
+//   return (
+//     <div className="py-2 flex flex-col overflow-auto">
+//         <Conversation />
+//     </div>
+//   )
+// }
+
+// export default Conversations
