54750467723adcf62541fb2b86b1731ef3924524
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-17 18:50:18 +0100

    ok 4th commit

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,20 +1,20 @@
-const Conversation = () => {
+const Conversation = ({conversation, lastIdx, emoji}) => {
   return (
     <>
         <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
             <div className="avatar online">
                 <div className="w-12 rounded-full">
-                    <img src="" alt="user avatar" />
+                    <img src={conversation.profilePic} alt="user avatar" />
                 </div>
             </div>
             <div className="flex flex-col flex-1">
                 <div className="flex gap-3 justify-between">
-                    <p className="font-bold text-gray-200">John Doe</p>
-                    <span className="text-x1">😂</span>
+                    <p className="font-bold text-gray-200">{conversation.fullName}</p>
+                    <span className="text-x1">{emoji}</span>
                 </div>
             </div>
         </div>
-        <div className="divider my-0 py-0 h-1" />
+        {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
   )
 }
