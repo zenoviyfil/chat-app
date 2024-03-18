c9ee686b0265d8e3ea9e55e7fdd8a803f413071e
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-18 10:56:29 +0100

    scroll bar

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,14 +1,24 @@
+import { useEffect, useRef } from "react"
 import useGetMessages from "../../hooks/useGetMessages"
 import MessageSkeleton from "../skeletons/MessageSkeleton"
 import Message from "./Message"
 
 const Messages = () => {
   const {messages, loading} = useGetMessages()
-  console.log("MESSAGES:", messages)
+  const lastMessageRef = useRef()
+
+  useEffect(() => {
+    lastMessageRef.current?.scrollIntoView({behavior: "smooth"})
+  }, [])
+
   return (
     <div className="px-4 flex-1 overflow-auto">
       {!loading && messages.length > 0 && messages.map((message) => (
-        <Message key={message._id} message={message} />
+        <div key={message._id}
+          ref={lastMessageRef}
+        >
+          <Message message={message} />
+        </div>
       ))}
 
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
